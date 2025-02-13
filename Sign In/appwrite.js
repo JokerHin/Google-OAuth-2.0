ccba44b83b0d6560d0e6ff4f763ef8de8a01550c
JokerHin <chokarhin@gmail.com>
2025-02-13 16:22:00 +0800

    update no mode

--- Sign In/appwrite.js
+++ Sign In/appwrite.js
@@ -4,8 +4,9 @@ const { VITE_PROJECT_ID, VITE_ENDPOINT } = import.meta.env;
 console.log(VITE_PROJECT_ID, VITE_ENDPOINT);
 
 const client = new Client()
-    .setEndpoint(VITE_ENDPOINT)
-    .setProject(VITE_PROJECT_ID);
+  .setEndpoint(VITE_ENDPOINT)
+  .setProject(VITE_PROJECT_ID)
+  .setMode("no-cors"); // Add this line to set the request's mode to 'no-cors'
 
 const account = new Account(client);
 
